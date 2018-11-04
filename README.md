a1d3b80fe746a4236be482fc117ef7a70ba971d0
R.A. Lucas <richard@richardalucas.com>
2018-11-04 13:22:44 -0700

    scaffold commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# colorization
+# Colorization
---
+++ colorize.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import scipy as sp
+import cv2
+import random
+
+class Colorize:
+  def __init__(self, images):
+    self.images = images
+
+
